27e78bd9f39e628443323c53adec62009a544441
Vizena2005 <vizena2005@gmail.com>
2018-04-16 13:24:42 -0500

    finshing schema and updating rows with dummy data

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -11,3 +11,18 @@ CREATE TABLE products(
   stock_quantity INT default 0,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("MagicEraser", "cleaning", 4.00, 7),
+    ("Wheel", "automotive", 250.00, 4),
+    ("Microphone", "entertainment", 150.50, 9),
+    ("Headphones", "entertainment", 124.99, 6),
+    ("Mouse", "entertainment", 115.00, 2),
+    ("Headphones", "entertainment", 74.56, 11),
+    ("Bike", "excersise", 82.00, 4),
+    ("Running Shoes", "apparel", 14.37, 16),
+    ("Shirt", "apparel", 11.24, 7);
+
+    
